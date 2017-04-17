1f5e2456fe489067db716c2833f96baac722cc76
maxime <maxime.betrisey@hotmail.com>
2017-04-17 14:50:14 +0200

    acceuil fini

--- app/src/main/java/ch/hevs/design/data/Vin.java
+++ app/src/main/java/ch/hevs/design/data/Vin.java
@@ -1,12 +1,19 @@
 package ch.hevs.design.data;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import java.util.ArrayList;
+
+import ch.hevs.design.R;
 
 /**
  * Created by hugo on 06.04.2017.
  */
 
 public class Vin {
+    private String img = "";
+    private String name;
     private int annee;
     private Couleur couleur;
     private Region region;
@@ -28,5 +35,35 @@ public class Vin {
     }
     public void setFournisseur(Fournisseur fournisseur){
         this.fournisseur = fournisseur;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+
+    public Vin(String img, String name, int annee){
+        this.img = img;
+        this.name = name;
+        this.annee = annee;
     }
 }
--- app/src/main/java/ch/hevs/design/fragments/HomeFragment.java
+++ app/src/main/java/ch/hevs/design/fragments/HomeFragment.java
@@ -1,13 +1,20 @@
 package ch.hevs.design.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ch.hevs.design.R;
+import ch.hevs.design.adapter.WineAdapter;
+import ch.hevs.design.data.Vin;
 
 /**
  * Created by maxim on 11.04.2017.
@@ -21,6 +28,35 @@ public class HomeFragment extends Fragment {
         View rootView = inflater.inflate(
                 R.layout.fragment_home, container, false);
 
+        mListView = (ListView) rootView.findViewById(R.id.listViewWine);
+
+        List<Vin> tweets = genererVins();
+
+        WineAdapter adapter = new WineAdapter(rootView.getContext(), tweets);
+        mListView.setAdapter(adapter);
+
         return rootView;
+    }
+    ListView mListView;
+
+    private List<Vin> genererVins(){
+        List<Vin> vins = new ArrayList<Vin>();
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        vins.add(new Vin("","Vin1",2000));
+        return vins;
     }
 }
